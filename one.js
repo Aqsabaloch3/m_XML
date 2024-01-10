8a84f1dfce7b5865eedd968c6dee41667b66a9b2
Fatima Binte Aqeel <binteaqeel2004@gmail.com>
2024-01-10 18:18:21 +0500

    one commit

---
+++ one.js
@@ -0,0 +1,25 @@
+var organization =[
+    {
+        "id" : 0,
+        "name" : "abc",
+        "designation" : "Manager",
+        "salary" : 50000,
+        "phone-no" : 49723895 
+    },
+
+    {
+        "id" : 1,
+        "name" : "xyz",
+        "designation" : "Gneral Manager",
+        "salary" : 30000,
+        "phone-no" : 4745995 
+    },
+
+    {
+        "id" : 2,
+        "name" : "abc",
+        "designation" : "HR",
+        "salary" : 40000,
+        "phone-no" : 49354695 
+    },
+]
